4122f81a60c9eb78cd38cd81a6655657502534fe
IliaBu <bunamaksi@yandex.ru>
2022-12-04 11:45:22 +0500

    Add folder and code Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 3: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да  */
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 7 == 0 && number1 % 23 == 0 ) {
+Console.WriteLine(Convert.ToString(number1) + " -> да");
+}
+else {
+Console.WriteLine(Convert.ToString(number1) + " -> нет");
+}
